072737d54b8801c7183f468c84d9874ed6b1cddf
yiyuxiaoliu <18516115393@163.com>
2019-05-31 14:19:54 +0800

    python中装饰器代替set get方法

--- 练习_Test/面向对象.py
+++ 练习_Test/面向对象.py
@@ -30,13 +30,16 @@ class liu(Get_first):
         # 覆写父类的方法
     def speak(self):
         print(self.name , self.age, self.grade)
-
+    @classmethod   # 类方法
+    def b_print(cls):  # 当前类
+        print(cls.__name__)  # 获取当前类
 # # 实例化类子类
 # s = liu('ken', 10, 60, 3)
 # s.speak()
 # 实例化父类
 a = Get_first('liu', 10 , 20)  # 私用变量不能从其他类里面调用，
 a.speak()
+liu.b_print()  # 调用类方法
 
 # 另一个类，多重继承之前的准备
 class zhen():
@@ -68,6 +71,32 @@ class Chid(Mothon):
         print('调用子类方法')
 c = Chid()
 c.MyMothod()
+
+# python装饰器代替set get方法
+# 第一种；变量的访问和设置，我们可以使用get、set方法
+class student():
+    def __init__(self, name):
+        self.__name = name
+    def get_name(self):   # 获得
+        return self.__name
+    def set_name(self, name):  # 设置
+        self.__name = name
+std = student('liuzhen')
+print(std.get_name())
+
+#方法二，用@语法糖，使得我们要使用set get功能时只需要通过.xx的形式即可
+class Student():
+    def __init__(self, body):
+        self.__name = body
+    @property
+    def name(self):
+        return self.__name
+    @name.setter # 和方法名要相同
+    def name(self, name):
+        self.__name = name
+std1 = Student('zhendada')
+print(std1.name)
+
 
 
 
